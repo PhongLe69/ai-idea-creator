6dfdd304a97ebabefedbd0faffd5669e958654ee
thuongtruong109 <thuongtruongofficial@gmail.com>
2025-03-09 00:47:12 +0700

    add assistant bot

---
+++ app/dashboard/assistant/page.tsx
@@ -0,0 +1,14 @@
+import Chatbox from "@/components/Chatbot";
+import React from "react";
+
+const page = () => {
+  return (
+    <iframe
+      src={`https://phongle69.github.io/ai-idea-creator?key=${process.env.NEXT_PUBLIC_GEMINI_API_KEY}`}
+      width="100%"
+      height="100%"
+    ></iframe>
+  );
+};
+
+export default page;
